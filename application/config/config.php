c6225102bf9cd0c93571150ba2ff09d015d46212
Sufian Qayyum <qsufian@gmail.com>
2017-10-12 00:57:01 +0500

    changed name to ackamarackus

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8050/macrosmatic/';
+$config['base_url'] = 'http://localhost:8050/ackamarackus/';
 
 /*
 |--------------------------------------------------------------------------
