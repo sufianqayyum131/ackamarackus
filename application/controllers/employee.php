6a5821421d5981990fb90e1bd3212b0868ca0827
Sufian Qayyum <Sufian.Qayyum@trovicor.com>
2017-10-14 03:20:43 +0500

    view employee

--- application/controllers/employee.php
+++ application/controllers/employee.php
@@ -17,7 +17,7 @@ class Employee extends CI_Controller {
 		$data['footer'] = 'template/footer';
 		$this -> load -> view('template/template', $data);
 	}
-	
+
 	function functionToTestgetAndSaveEmployeeDetailsResult() {
 
 		$result = $this -> getMaxIDFromEmployeeDetails();
@@ -320,6 +320,35 @@ class Employee extends CI_Controller {
 			//$this -> load -> view('upload_success', $data);
 		}
 
+	}
+
+	function getAllEmployeesFromDatabase() {
+
+		$list= $this -> employee_model -> getAllEmployees();
+		if (is_array($list) === true && count($list) > 0) {
+			$data = array();
+			foreach ($list as $customers) {
+				$row = array();
+				$row['employeeID'] = $customers -> employeeID;
+				$row['name'] = $customers -> firstName . " " . $customers -> lastName;
+				$row['userName'] = $customers -> userName;
+				$row['email'] = $customers -> email;
+				$row['mobileNum'] = $customers -> mobileNum;
+				$row['department'] = $customers -> department;
+				$row['designation'] = $customers -> designation;
+				$row['employeeType'] = $customers -> employeeType;
+				$row['supervisorID'] = $customers -> supervisorID;
+				$data[] = $row;
+			}
+
+			$successResponse = array("status" => "true", "msg" => "Teacher's Data Received", "data" => $data, );
+			echo json_encode($successResponse);
+
+		} else {
+			$errorResponse = array("status" => "false", "msg" => "Teachers Data Not Found");
+			echo json_encode($errorResponse);
+			
+		}
 	}
 
 }
--- application/models/employee_model.php
+++ application/models/employee_model.php
@@ -3,14 +3,12 @@ class Employee_model extends CI_Model {
 
 	function createEmployee($employeeID, $firstName, $middleName, $lastName, $userName, $email, $mobileNum, $homePhone, $cnicNumber, $dob, $address, $emergencyContactNumber, $emergencyContactName, $bloodGroup, $father_husbandName, $hireDate, $profilePic, $resume) {
 
-		$status = $this -> db -> insert('employee_basic_details', array('employeeID' => $employeeID, 'firstName' => $firstName, 
-		'middleName' => $middleName, 'lastName' => $lastName, 'userName' => $userName, 
-		'email' => $email, 'mobileNum' => $mobileNum, 'cnicNumber' => $cnicNumber, 'homePhone'=>$homePhone,'dob' => $dob, 'address' => $address, 'emergencyContactName' => $emergencyContactName, 'emergencyContactNumber' => $emergencyContactNumber, 'bloodGroup' => $bloodGroup, 'father_husbandName' => $father_husbandName, 'hireDate' => $hireDate, 'profilePic' => $profilePic, 'resume' => $resume));
+		$status = $this -> db -> insert('employee_basic_details', array('employeeID' => $employeeID, 'firstName' => $firstName, 'middleName' => $middleName, 'lastName' => $lastName, 'userName' => $userName, 'email' => $email, 'mobileNum' => $mobileNum, 'cnicNumber' => $cnicNumber, 'homePhone' => $homePhone, 'dob' => $dob, 'address' => $address, 'emergencyContactName' => $emergencyContactName, 'emergencyContactNumber' => $emergencyContactNumber, 'bloodGroup' => $bloodGroup, 'father_husbandName' => $father_husbandName, 'hireDate' => $hireDate, 'profilePic' => $profilePic, 'resume' => $resume));
 		if ($status) {
-		
+
 			return $status;
 		} else {
-			
+
 			$error = $this -> db -> error();
 
 			// If an error occurred, $error will now have 'code' and 'message' keys...
@@ -65,31 +63,30 @@ class Employee_model extends CI_Model {
 	function createJobHistory($jobHistory) {
 
 		foreach ($jobHistory as $a) {
-			$data = array('employeeID' => $a['employeeID'], 'company' => $a['company'], 'designation' => $a['designation'], 
-			'employmentStartDate' => $a['employmentStartDate'], 'employmentEndDate' => $a['employmentEndDate']);
+			$data = array('employeeID' => $a['employeeID'], 'company' => $a['company'], 'designation' => $a['designation'], 'employmentStartDate' => $a['employmentStartDate'], 'employmentEndDate' => $a['employmentEndDate']);
 
 			$save = $this -> db -> insert('employee_job_history', $data);
-			
-			 }if ($save) {
-				return $save;
-			} else {
-				$error = $this -> db -> error();
 
-				// If an error occurred, $error will now have 'code' and 'message' keys...
-				if (isset($error['message'])) {
-					return $error['message'];
-				}
+		}
+		if ($save) {
+			return $save;
+		} else {
+			$error = $this -> db -> error();
 
-				// No error returned by the DB driver...
-				return null;
+			// If an error occurred, $error will now have 'code' and 'message' keys...
+			if (isset($error['message'])) {
+				return $error['message'];
 			}
+
+			// No error returned by the DB driver...
+			return null;
+		}
 	}
 
 	function createEducation($education) {
 
 		foreach ($education as $a) {
-			$data = array('employeeID' => $a['employeeID'], 'instituteName' => $a['instituteName'], 'qualification' => $a['qualification'], 
-			'admissionDate' => $a['admissionDate'], 'graduationDate' => $a['graduationDate']);
+			$data = array('employeeID' => $a['employeeID'], 'instituteName' => $a['instituteName'], 'qualification' => $a['qualification'], 'admissionDate' => $a['admissionDate'], 'graduationDate' => $a['graduationDate']);
 
 			$save = $this -> db -> insert('employee_education_history', $data);
 			/*echo "<pre>";
@@ -116,9 +113,7 @@ class Employee_model extends CI_Model {
 	function createTraining($training) {
 
 		foreach ($training as $a) {
-			$data = array('employeeID' => $a['employeeID'], 'trainingInstituteName' => $a['trainingInstituteName'], 
-			'trainingStartDate' => $a['trainingStartDate'], 'trainingEndDate' => $a['trainingEndDate'], 'ExamDate' => $a['ExamDate'], 
-			 'certificationName' => $a['certificationName']);
+			$data = array('employeeID' => $a['employeeID'], 'trainingInstituteName' => $a['trainingInstituteName'], 'trainingStartDate' => $a['trainingStartDate'], 'trainingEndDate' => $a['trainingEndDate'], 'ExamDate' => $a['ExamDate'], 'certificationName' => $a['certificationName']);
 
 			$save = $this -> db -> insert('employee_trainings_history', $data);
 			/*echo "<pre>";
@@ -126,19 +121,20 @@ class Employee_model extends CI_Model {
 			 echo "<pre>";
 			 echo "ends here";*/
 
-		}if ($save) {
-				return $save;
-			} else {
-				$error = $this -> db -> error();
+		}
+		if ($save) {
+			return $save;
+		} else {
+			$error = $this -> db -> error();
 
-				// If an error occurred, $error will now have 'code' and 'message' keys...
-				if (isset($error['message'])) {
-					return $error['message'];
-				}
-
-				// No error returned by the DB driver...
-				return null;
+			// If an error occurred, $error will now have 'code' and 'message' keys...
+			if (isset($error['message'])) {
+				return $error['message'];
 			}
+
+			// No error returned by the DB driver...
+			return null;
+		}
 	}
 
 	function updateIdOfLastEmployeeAddedAfterSucessfullEmployeeAddition($employeeID, $versionBit) {
@@ -280,6 +276,17 @@ class Employee_model extends CI_Model {
 			return "there must be data missing in child tables";
 		}
 
+	}
+
+	function getAllEmployees() {
+		$this -> db -> select('employee_basic_details.*,employee_department_details.*');
+		$this -> db -> from('employee_basic_details');
+		$this -> db -> join('employee_department_details', 'employee_basic_details.employeeID = employee_department_details.employeeID', 'left');
+		//$this->db->where('employee_basic_details.employeeID', 1014);
+		$query = $this -> db -> get();
+		
+		return $query->result();
+		
 	}
 
 }
